4a6daf3e9fef61c2742022219dcd44656e1a727c
Eduardo San Martin Morote <posva13@gmail.com>
2016-07-27 22:07:39 +0200

    wip

---
+++ assets/js/src/components/searchbox.js
@@ -0,0 +1,27 @@
+import fetchival from 'fetchival'
+
+const search = fetchival('https://EWN7R5RMAA-dsn.algolia.net/1/indexes/posva_blog', {
+  headers: {
+    'X-Algolia-API-Key': 'c089b4fed98dc56dae0ceecf48cb3f6f',
+    'X-Algolia-Application-ID': 'EWN7R5RMAA'
+  }
+})
+
+export default {
+  data: function () {
+    return {
+      query: ''
+    }
+  },
+  watch: {
+    query: function (query) {
+      const me = this
+      me.queryId = me.queryId || 0
+      const queryId = ++me.queryId
+      search.get({query: query})
+        .then(function (results) {
+          if (queryId === me.queryId) me.$emit('hits', results.hits)
+        })
+    }
+  }
+}
--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,1 +1,13 @@
+import Vue from 'vue/dist/vue.min.js'
 import './canvas.js'
+import Searchbox from './components/searchbox.js'
+
+Vue.config.delimiters = ['${', '}']
+Vue.component('searchbox', Searchbox)
+
+window.vm = new Vue({
+  el: 'body',
+  data: {
+    hits: []
+  }
+})
